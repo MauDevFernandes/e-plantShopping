c5804b493771256ff614f06f6197bff1b1e7c7a4
MauDevFernandes <35377601+MauDevFernandes@users.noreply.github.com>
2025-02-06 13:38:04 -0500

    first changes

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,6 +9,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+    
  
   };
 
@@ -31,6 +32,9 @@ const CartItem = ({ onContinueShopping }) => {
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
   };
+  const handleCheckoutShopping = (e) => {
+  alert('Functionality to be added for future reference');
+};
 
   return (
     <div className="cart-container">
@@ -57,7 +61,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={() => handleCheckoutShopping(e)>Checkout</button>
       </div>
     </div>
   );
